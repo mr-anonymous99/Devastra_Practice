81844d756cf40173216ef25834c10e89523eb348
Mrcatman <unknownmister132@gmail.com>
2025-09-06 15:17:02 +0530

    some doubts are there

--- Javascript/prac.js
+++ Javascript/prac.js
@@ -1,2 +1,88 @@
 console.log("2"+ +"2");
 console.log("prem sir ");
+
+//MEMEs experiment
+
+console.log(2+ +"2");
+console.log(2 + 2 -2);
+console.log("2" + "2" -"2");
+
+
+
+
+// OPERATORS
+
+//assignment
+const a=7
+const b=9
+
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a>b);
+console.log("2" -"2");
+
+
+//Relational Operators
+// const k="vinay"
+// const l="vinay siringi"
+const  l=null
+const  k=1
+
+console.log(typeof(l));
+// console.log(l.values);
+
+console.log(k && l);
+console.log(l || k);
+console.log(l ?? k);
+console.log(l);
+
+// console.log(l ? l);
+
+//String Opeartors
+
+const firtName="vinay"
+const LastName="Siringi"
+
+console.log(firtName+LastName);
+
+
+//literals 
+//using the  ,backticks and the dollar symbol as
+console.log(`my name is ${firtName + " "+ LastName}`);
+
+//Terinary Operator
+let Prem=null;
+const hello=3;
+console.log(console.log(hello));
+
+if (Prem??=true) {
+    console.log("yeah it worked");
+    
+}
+
+//Increments and Decrements
+
+let count=5
+console.log(count++);
+console.log(count);
+console.log(++count);
+
+let obj={
+    name:"vinay",
+    roll_no:58
+}
+
+console.log(typeof([]+[]));
+console.log(typeof({}+{}));
+console.log(typeof({}+[]));
+
+let vinay =new Array(5,7)
+let ganesh=Object.create(obj)
+console.log(Object.entries(obj));
+
+console.log(typeof(vinay));
+
+
