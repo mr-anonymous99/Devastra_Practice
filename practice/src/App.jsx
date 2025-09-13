84f6ae240d52ee766eadce25b86ec00ccd0af989
Mrcatman <unknownmister132@gmail.com>
2025-09-13 17:10:51 +0530

    form data capturing is done

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -11,43 +11,44 @@ function App() {
   const [count, setCount] = useState(0)
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
+  const [formData,setFormData]=useState({
+    email:"",
+    password:""
+  })
 
-  
-  const handleClicked = (name) => {
-    console.log("you clicked a button");
-    alert(`ohh ${name} you clicked a button !!!`)
+
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
   }
-  const handleIncrement = () => {
-    setCount((k) => (k + 1))
-  }
-  //by deafault every input field returns a object.target.value and object.target.name
-  const handleChange = (e) => {
-    console.log(e);
-    console.log(e.target.name + e.target.value);
+  const handleEmail=(e)=>{
+    // setEmail((mail)=>(email=e.target.value))
+    setEmail(e.target.value)
   }
 
+  const handlePassword=(e)=>{
+    console.log("email is " + e.target.value);
+    // setPassword((pass)=>(password=))
+    setPassword(e.target.value)
+  }
   const handleSubmit = (e) => {
     // console.log(e);
-
     //this can make the browser not to refresh after the submit
     e.preventDefault()
+    console.log(`email :${formData.email} \nPassword : ${formData.password}`); 
     alert("form submitted !!")
   }
+  
   return (
     <div >
       <p>i am a button</p>
-      <button onClick={() => (handleClicked("vinay"))}>Click me</button>
-      <form action="/post" onSubmit={handleSubmit}>
-        {/* <input type="text" name="my name" placeholder="type your name" /> */}
-        {/* <input type="text" name="my name" onChange={handleChange} placeholder="type your name" /> */}
-        <input type="password" name="passowrd" placeholder="enter your password" />
-        <input type="email" name="email" placeholder="enter your email" />
+      <form onSubmit={handleSubmit}>
+        <input type="password" name="password" onChange={handleChange} placeholder="enter your password" />
+        <input type="email" name="email"  onChange={handleChange}placeholder="enter your email" />
         <button type="submit" >Submit</button>
       </form>
-
-      <h1>count : {count}</h1>
-      <button onClick={() => count < 10 ? setCount(count + 1) : alert("you cant increament greater than 10")}>increment</button>
-      <button onClick={() => (count > 0 ? setCount(count - 1) : alert("cannot decrement <0 "))}>decrement</button>
     </div>
   )
 }
