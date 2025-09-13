0ee96353ff85ecfbfcefce65b35d7351d2d71eb4
Mrcatman <unknownmister132@gmail.com>
2025-09-13 16:38:03 +0530

    usestate demo finished

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Profile from "./Profile.jsx"
 import ProductCard from "./Productcard"
 import Greeting from "./Greeting";
@@ -5,92 +7,47 @@ import Idcard from "./Idcard";
 import Blogpost from "./Blogpost"
 import "./Greeting.css"
 function App() {
-    const ProductName = "Banana";
-    const Price = 500;
-    const IsAvalable = true;
-    const notavailable=false
 
-    const obj1={
-      name:"Drakamora",
-      course:"B-tech",
-      dob:"12/7/2025",
-      roll_no:"23VV1A0518",
-      blood_group:"O +ve",
-      place:"north America",
-      img:"./meme2.png"
-    }
-    const obj2={
-      name:"Batman",
-      course:"B-tech",
-      dob:"12/9/2025",
-      roll_no:"23VV1A0548",
-      blood_group:"O -ve",
-      place:"Gotham Foundations",
-      img:"./meme3.jpg"
-    }
+  const [count, setCount] = useState(0)
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
 
-    //Array of objects Declaration cause all of the data coming form the server is in the form of an Array of objects
-
-    const product=[{
-      Productname:"banana",
-      price:100,
-      isAvailable:true
-    },{
-      Productname:"Apple",
-      price:1000,
-      isAvailable:false
-    },{
-      Productname:"kiwi",
-      price:190,
-      isAvailable:false
-    },{
-      Productname:"pomogranate",
-      price:200,
-      isAvailable:true
-    }]
-
-    const Blog=[{
-      id:1,
-      authour:"Vemana",
-      title:"A Coward",
-      description:"A poem goes like medipandu chudu melimai undu pttavippi chudu purugulundu"
-    },{
-      id:2,
-      authour:"Trump",
-      title:"A tariff tragedy",
-      description:"yeah this book tell about the tariff exports based on the trump politics"
-    },{
-      id:3,
-      authour:"Charlie",
-      title:"A brave soldier",
-      description:"an inspiring story about the charlie who fought against the LGBTQ and feminism in America !!"
-    }]
   
+  const handleClicked = (name) => {
+    console.log("you clicked a button");
+    alert(`ohh ${name} you clicked a button !!!`)
+  }
+  const handleIncrement = () => {
+    setCount((k) => (k + 1))
+  }
+  //by deafault every input field returns a object.target.value and object.target.name
+  const handleChange = (e) => {
+    console.log(e);
+    console.log(e.target.name + e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    // console.log(e);
+
+    //this can make the browser not to refresh after the submit
+    e.preventDefault()
+    alert("form submitted !!")
+  }
   return (
-    <div>
-      {/* <Profile/> */}
-      {/* <ProductCard Pname="Banana" price={Price} available={IsAvalable}/>
-      <ProductCard Pname="AratiPandu" price="1000" available={notavailable}/> */}
-      
-      {/* <Idcard object={obj1}/> */}
-      {/* <Idcard object={obj2}/> */}
+    <div >
+      <p>i am a button</p>
+      <button onClick={() => (handleClicked("vinay"))}>Click me</button>
+      <form action="/post" onSubmit={handleSubmit}>
+        {/* <input type="text" name="my name" placeholder="type your name" /> */}
+        {/* <input type="text" name="my name" onChange={handleChange} placeholder="type your name" /> */}
+        <input type="password" name="passowrd" placeholder="enter your password" />
+        <input type="email" name="email" placeholder="enter your email" />
+        <button type="submit" >Submit</button>
+      </form>
 
-
-      {/* this is done all thwe way at last using maps */}
-       {/* {product.map((p)=>(
-        <ProductCard Pname={p.Productname} Price={p.price} available={p.isAvailable}/>
-       ))} */}
-
-       {/* {
-        Blog.map((b)=>(
-          //the addition od id is due to telling that on newly adding the previous will not be reloaded  again
-          <Blogpost key={b.id} authour={b.authour} title={b.title} description={b.description}/>
-        ))
-       } */}
-
-
-
-       
+      <h1>count : {count}</h1>
+      <button onClick={() => count < 10 ? setCount(count + 1) : alert("you cant increament greater than 10")}>increment</button>
+      <button onClick={() => (count > 0 ? setCount(count - 1) : alert("cannot decrement <0 "))}>decrement</button>
     </div>
   )
 }
